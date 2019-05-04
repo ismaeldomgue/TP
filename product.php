2ce5d948843b19a6e08eedf9cf72aa29a45f66ad
ismaeldomgue <ismaeldomgue@gmail.com>
2019-05-04 15:44:11 +0300

    Ajout du plugin JQuery

---
+++ product.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Page Produit</title>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+    <script src="jquery.simpleplaceholder.js" type="text/javascript"></script>
+  </head>
+  <body>
+    <h1> Product page </h1>
+  </body>
+</html>
